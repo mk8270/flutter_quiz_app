60f7587d7a55cc190bbd28ff35919799cd4b9fe2
mk8270 <manikandan8270m@gmail.com>
2023-11-14 13:15:09 +0530

    start screen 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 33, 9, 75),
+                Color.fromARGB(255, 157, 124, 214),
+              ],
+            ),
+          ),
+          child: const StratScreen(),
+        ),
+      ),
+    ),
+  );
+}
